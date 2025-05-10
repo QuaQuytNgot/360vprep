237b370b426f2d349dd29d36ca0010bd247af845
HoangBui <huyhoang0160072k5@gmail.com>
2025-05-10 15:07:22 +0700

    Update encode_HEVC.sh

--- encode_HEVC.sh
+++ encode_HEVC.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
-VNAME="Diving_10s"
-TILE_YUV_PATH="${VNAME}/erp_4x3/tile_yuv"	#change here
-TILE_LOG_PATH="${VNAME}/erp_4x3/tile_log"	#change here
-W_yuv=3840
-H_yuv=1920
-No_tile_W=4                             	    	
-No_tile_H=3        
-W_TILE=960                              		#W_yuv/No_tile_W  
-H_TILE=640						#H_yuv/No_tile_H 
+VNAME="Diving"
+W_TILE=960                                # W_yuv / No_tile_W
+H_TILE=640                                # H_yuv / No_tile_H
 FRAME_RATE=30
+FRAMES=30                                 
 QP_LIST=(38 32 28 24 20)
 
-mkdir -p "${TILE_LOG_PATH}"
-
-for tile in "${TILE_YUV_PATH}"/tile_*.yuv; do
+find "${VNAME}" -type f -path "*/erp_4x3/tile_yuv/tile_*.yuv" | while read -r tile; do
     if [[ ! -f "$tile" ]]; then
-        echo "Can not find .yuv in  ${TILE_YUV_PATH}"
-        exit 1
+        echo "Cannot find .yuv tile file: $tile"
+        continue
     fi
+
+    TILE_DIR=$(dirname "$tile")
+    LOG_DIR="${TILE_DIR/\/tile_yuv/\/tile_log}"
+
+    mkdir -p "$LOG_DIR"
 
     for QP in "${QP_LIST[@]}"; do
         BASENAME=$(basename "${tile%.yuv}")
-        OUTPUT="${TILE_YUV_PATH}/${BASENAME}_QP${QP}.bin"
-        LOG="${TILE_LOG_PATH}/log_encode_${BASENAME}_QP${QP}.txt"
+        OUTPUT="${TILE_DIR}/${BASENAME}_QP${QP}.bin"
+        LOG="${LOG_DIR}/log_encode_${BASENAME}_QP${QP}.txt"
 
         echo "Encoding ${tile} with QP=${QP}"
-        x265 --input "${tile}" \
+        x265 --input "$tile" \
              --input-res "${W_TILE}x${H_TILE}" \
              --fps "${FRAME_RATE}" \
-             --frames 300 \
+             --frames "${FRAMES}" \
              --qp "${QP}" \
              --preset medium \
-             --output "${OUTPUT}" \
-             > "${LOG}" 2>&1
+             --output "$OUTPUT" \
+             > "$LOG" 2>&1
 
         if [[ $? -eq 0 ]]; then
-            echo "Finishing encoding ${OUTPUT}"
+            echo "Finished encoding ${OUTPUT}"
         else
             echo "Error encoding ${tile} with QP=${QP}, check log: ${LOG}"
             exit 1
@@ -44,4 +42,4 @@ for tile in "${TILE_YUV_PATH}"/tile_*.yuv; do
     done
 done
 
-echo "Finish encoding"
+echo "Finished encoding all tiles."
